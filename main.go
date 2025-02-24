63842e0beac2d0c6e0593a6e330836bab5529e9c
Adam Fidel <adam@fidel.id>
2025-02-24 16:45:25 -0600

    feat: implement apply and revert

--- main.go
+++ main.go
@@ -1,47 +1,47 @@
 package main
 
 import (
-	"fmt"
-	"github.com/spf13/cobra"
-	"os"
+    "fmt"
+    "github.com/spf13/cobra"
+    "os"
 )
 
 func main() {
-	var rootCmd = &cobra.Command{
-		Use:   "xfconf-profile",
-		Short: "A CLI tool for managing Xfce profiles",
-	}
+    var rootCmd = &cobra.Command{
+        Use:   "xfconf-profile",
+        Short: "A CLI tool for managing Xfce profiles",
+    }
 
-	var applyCmd = &cobra.Command{
-		Use:   "apply [path]",
-		Short: "Apply changes from a profile.json",
-		Args:  cobra.ExactArgs(1),
-		Run: func(cmd *cobra.Command, args []string) {
-			applyProfile(args[0])
-		},
-	}
+    var applyCmd = &cobra.Command{
+        Use:   "apply [path]",
+        Short: "Apply changes from a profile.json",
+        Args:  cobra.ExactArgs(1),
+        Run: func(cmd *cobra.Command, args []string) {
+            applyProfile(args[0])
+        },
+    }
 
-	var revertCmd = &cobra.Command{
-		Use:   "revert [path]",
-		Short: "Revert changes from a profile.json",
-		Args:  cobra.ExactArgs(1),
-		Run: func(cmd *cobra.Command, args []string) {
-			revertProfile(args[0])
-		},
-	}
+    var revertCmd = &cobra.Command{
+        Use:   "revert [path]",
+        Short: "Revert changes from a profile.json",
+        Args:  cobra.ExactArgs(1),
+        Run: func(cmd *cobra.Command, args []string) {
+            revertProfile(args[0])
+        },
+    }
 
-	var recordCmd = &cobra.Command{
-		Use:   "record",
-		Short: "Record changes to xsettings and dump them as a profile on SIGINT",
-		Run: func(cmd *cobra.Command, args []string) {
-			recordProfile()
-		},
-	}
+    var recordCmd = &cobra.Command{
+        Use:   "record",
+        Short: "Record changes to xsettings and dump them as a profile on SIGINT",
+        Run: func(cmd *cobra.Command, args []string) {
+            recordProfile()
+        },
+    }
 
-	rootCmd.AddCommand(applyCmd, revertCmd, recordCmd)
+    rootCmd.AddCommand(applyCmd, revertCmd, recordCmd)
 
-	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+    if err := rootCmd.Execute(); err != nil {
+        fmt.Println(err)
+        os.Exit(1)
+    }
 }
--- profile.go
+++ profile.go
@@ -1,15 +1,70 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os/exec"
+)
 
-func applyProfile(profilePath string) {
-    fmt.Printf("Applying profile %s\n", profilePath)
+func applyProfile(profilePath string) error {
+  fmt.Printf("Applying profile %s\n", profilePath)
+
+	data, err := ioutil.ReadFile(profilePath)
+	if err != nil {
+		return fmt.Errorf("failed to read file: %v", err)
+	}
+
+	var profile map[string]map[string]interface{}
+	err = json.Unmarshal(data, &profile)
+	if err != nil {
+		return fmt.Errorf("failed to parse JSON: %v", err)
+	}
+
+	for channel, properties := range profile {
+		for property, value := range properties {
+			cmd := exec.Command("xfconf-query", "-c", channel, "--property", property, "--set", fmt.Sprintf("%v", value))
+
+			output, err := cmd.CombinedOutput()
+			if err != nil {
+				return fmt.Errorf("failed to run command: %v\nOutput: %s", err, string(output))
+			}
+
+			fmt.Printf("Executed: %s\nOutput: %s\n", cmd.String(), string(output))
+		}
+	}
+
+	return nil
 }
 
-func revertProfile(profilePath string) {
-    fmt.Printf("Reverting profile %s\n", profilePath)
+func revertProfile(profilePath string) error {
+	data, err := ioutil.ReadFile(profilePath)
+	if err != nil {
+		return fmt.Errorf("failed to read file: %v", err)
+	}
+
+	var profile map[string]map[string]interface{}
+	err = json.Unmarshal(data, &profile)
+	if err != nil {
+		return fmt.Errorf("failed to parse JSON: %v", err)
+	}
+
+	for channel, properties := range profile {
+		for property := range properties {
+			cmd := exec.Command("xfconf-query", "-c", channel, "--reset", "--property", property)
+
+			output, err := cmd.CombinedOutput()
+			if err != nil {
+				return fmt.Errorf("failed to run command: %v\nOutput: %s", err, string(output))
+			}
+
+			fmt.Printf("Executed: %s\nOutput: %s\n", cmd.String(), string(output))
+		}
+	}
+
+	return nil
 }
 
 func recordProfile() {
-    fmt.Printf("Recording profile...\n")
+    fmt.Printf("TODO\n")
 }
