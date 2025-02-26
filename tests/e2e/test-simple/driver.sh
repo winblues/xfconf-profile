be3bf71984d9935d3bbcab6cd91013b6536708ad
Adam Fidel <adam@fidel.id>
2025-02-26 10:37:12 -0600

    test: start of end-to-end tests

---
+++ tests/e2e/test-simple/driver.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -euo pipefail
+
+xfconf_query=$(which xfconf-query)
+
+if ! which xfconf-query | grep -q "/xfconf-profile/"; then
+  echo "ERROR: Using system's xfconf-query. Check your PATH."
+  exit 1
+fi
+
+xfconf-profile apply profile.json
+
+if diff expected-log.txt actual-log.txt; then
+  echo "Differences detected"
+  exit 1
+else
+  echo "Passed"
+  exit 0
+fi
